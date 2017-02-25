68ecf6c65eddb5fc28abee1008c88d2fe9ff3c91
Taras Pyvovarov <taras.pyvovarov@gmail.com>
2017-02-25 19:18:50 +0200

    Added MVC

---
+++ src/aspnet-core-sample2/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_core_sample2.Controllers
+{
+    [Route("")]
+    [Route("Home")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [Route(nameof(Index))]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- src/aspnet-core-sample2/Startup.cs
+++ src/aspnet-core-sample2/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -25,6 +24,7 @@ namespace aspnet_core_sample2
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,14 +33,10 @@ namespace aspnet_core_sample2
             loggerFactory.AddConsole();
 
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseMvc();
         }
     }
 }
