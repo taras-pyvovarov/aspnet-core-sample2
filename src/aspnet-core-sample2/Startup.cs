ad289f63d9ba79bcd807f620f42330d16016fafb
Taras Pyvovarov <taras.pyvovarov@gmail.com>
2017-02-25 18:00:33 +0200

    Project inited

--- src/aspnet-core-sample2/Startup.cs
+++ src/aspnet-core-sample2/Startup.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -10,8 +7,20 @@ using Microsoft.Extensions.Logging;
 
 namespace aspnet_core_sample2
 {
-    public class Startup
+    internal class Startup
     {
+        private static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
